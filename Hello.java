9015df3a585f419d609a10fb3f2a76bf613b5c96
unknown <suyogpandagare17@gmail.com>
2022-06-04 16:19:44 +0530

    Bank operation functionality added

--- Hello.java
+++ Hello.java
@@ -1,6 +1,18 @@
 package hello;
 //changes done
 public class Account{
+	
+	public void deposit(float amount){
+	
+	this.balance += amount;
+	
+	}
+	public void withdraw(float amount){
+	
+	this.balance -= amount;
+	
+	}
+	
     private float balance;
     public Account(){
         this.balance=5000;
